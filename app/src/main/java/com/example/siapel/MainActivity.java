4fb67b0989dc28fc01d3f6794b16259b385c00af
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-07-02 22:32:07 +0700

    refactor: mainActicity

--- app/src/main/java/com/example/siapel/MainActivity.java
+++ app/src/main/java/com/example/siapel/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.siapel;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int waktu_delay=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dashboard= new Intent(MainActivity.this,PelangganListAct.class);
+                startActivity(dashboard);
+                finish();
+            }
+        },waktu_delay);
     }
 }
