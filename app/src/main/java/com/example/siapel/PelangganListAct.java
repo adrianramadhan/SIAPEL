d3d88ffdaf04554e8b16677c98b55aee18dde5ca
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-07-02 22:05:10 +0700

    feat: pelanggan-list-act

--- app/src/main/java/com/example/siapel/PelangganListAct.java
+++ app/src/main/java/com/example/siapel/PelangganListAct.java
@@ -1,4 +1,196 @@
 package com.example.siapel;
 
-public class PelangganListAct {
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.siapel.adapter.PelangganAdapter;
+import com.example.siapel.model.PelangganModel;
+import com.example.siapel.services.PelangganService;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PelangganListAct extends AppCompatActivity {
+    ListView listView;
+    AlertDialog.Builder dialog;
+    List<PelangganModel> pelanggans=new ArrayList<>();
+    PelangganAdapter pelangganAdp;
+    PelangganService db=new PelangganService(this);
+    Button btnTambah;
+    TextInputEditText tiCari;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pelanggan_list);
+
+        listView=findViewById(R.id.lv_pelanggan);
+        btnTambah=findViewById(R.id.btn_Tambah);
+        tiCari=findViewById(R.id.tie_Cari);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pel=new Intent(PelangganListAct.this, PelangganAct.class);
+                startActivity(pel);
+            }
+        });
+
+        tiCari.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                getData(tiCari.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable e) {
+
+            }
+        });
+
+        pelangganAdp=new PelangganAdapter(PelangganListAct.this, pelanggans);
+        listView.setDivider(null);
+        listView.setAdapter(pelangganAdp);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final String kode=pelanggans.get(position).get_id_pel();
+                final String nama=pelanggans.get(position).get_nama_pel();
+                final String jenis=pelanggans.get(position).get_jenis();
+
+                final CharSequence[] dialogItem={"Ubah", "Hapus", "Copy"};
+                dialog=new AlertDialog.Builder(PelangganListAct.this);
+                dialog.setItems(dialogItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        switch (which){
+                            case 0:
+                                Intent pel=new Intent(PelangganListAct.this,
+                                        PelangganAct.class);
+                                pel.putExtra("id_pel", kode);
+                                pel.putExtra("nama_pel", nama);
+                                pel.putExtra("jenis", jenis);
+                                startActivity(pel);
+                                break;
+                            case 1:
+                                konfirmHapus(kode);
+                                break;
+                            case 2:
+                                copyToClipBoard(kode);
+                                tiCari.setText("");
+                                break;
+                        }
+                    }
+                }).show();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        pelanggans.clear();
+
+        if (tiCari.getText().equals("")){
+            getData();
+        }
+        else {
+            getData(tiCari.getText().toString());
+        }
+    }
+
+    public void getData(){
+        ArrayList<HashMap<String,String>> daftar=db.getAll();
+
+        pelanggans.clear();
+        for (int i=0;i< daftar.size();i++){
+            String kode = daftar.get(i).get("id_pel");
+            String nama = daftar.get(i).get("nama_pel"); // Mengubah nama variabel lokal
+            String jenis = daftar.get(i).get("jenis");
+
+            PelangganModel pelanggan = new PelangganModel();
+            pelanggan.set_id_pel(kode);
+            pelanggan.set_nama_pel(nama); // Menggunakan variabel yang sudah diubah
+            pelanggan.set_jenis(jenis);
+            pelanggans.add(pelanggan);
+        }
+        pelangganAdp.notifyDataSetChanged();
+    }
+
+    public void getData(String namaCari){
+        ArrayList<HashMap<String,String>> daftar=db.getAllByNama(namaCari);
+
+        pelanggans.clear();
+        for (int i=0;i< daftar.size();i++){
+            String kode=daftar.get(i).get("id_pel");
+            String nama=daftar.get(i).get("nama_pel"); // Mengubah nama variabel lokal
+            String jenis=daftar.get(i).get("jenis");
+
+            PelangganModel pelanggan = new PelangganModel();
+            pelanggan.set_id_pel(kode);
+            pelanggan.set_nama_pel(nama); // Menggunakan variabel yang sudah diubah
+            pelanggan.set_jenis(jenis);
+            pelanggans.add(pelanggan);
+        }
+        pelangganAdp.notifyDataSetChanged();
+    }
+
+    private void copyToClipBoard(String data){
+        String ambilData=data;
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(
+                Context.CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("copy data", ambilData);
+        clipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(this, "ID pelanggan di copy", Toast.LENGTH_SHORT).show();
+    }
+
+    private void konfirmHapus(String kode){
+        AlertDialog.Builder alert= new AlertDialog.Builder(this);
+
+        alert.setTitle("Konfirmasi Hapus")
+                .setMessage("Yakin data akan dihapus?")
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        db.delete(kode);
+                        pelanggans.clear();
+                        getData();
+                    }
+                })
+
+                .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false);
+        alert.show();
+    }
+
 }
